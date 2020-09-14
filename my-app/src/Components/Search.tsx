e3757d1263c339b239ddde7ff2f43dcf88fe7e00
Lance Delos Reyes <lan_drey19@hotmail.com>
2020-09-15 02:11:51 +1200

    HomePage and Components Folder and initial workings

---
+++ my-app/src/Components/Search.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Grid, TextField, Button } from '@material-ui/core';
+
+function Search(props : any) {
+
+
+}
+
+export default Search;
---
+++ my-app/src/Page/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Grid, TextField, Button } from '@material-ui/core';
+
+function HomePage() {
+    return (
+        <div>
+            <Button variant="outlined" color="primary">
+                Search by ID
+            </Button>
+        </div>
+    )
+}
+
+
+export default HomePage
